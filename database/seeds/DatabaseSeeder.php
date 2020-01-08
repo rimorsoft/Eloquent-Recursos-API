2ffa4449fe860d0d5e95197451777c57207c0c13
Profesor, Italo Morales F <profesoritalomoralesf@gmail.com>
2020-01-08 12:37:57 -0500

    configuración base

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 3)->create();
+        factory(App\Post::class, 18)->create();
     }
 }
