be2f6eca8e20689d3b50b824ec912e36a8a39ab3
Profesor, Italo Morales F <profesoritalomoralesf@gmail.com>
2020-01-08 12:52:28 -0500

    Recurso individual

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Post;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\Post as PostResources;
+
 class PostController extends Controller
 {
     /**
@@ -37,10 +39,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return [
-            'id' => $post->id,
-            'title' => $post->title,
-        ];
+        return new PostResources($post);
     }
 
     /**
